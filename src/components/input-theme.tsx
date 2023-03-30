e874df331c87af0f59bcd5e1535cbd6d1df8d2fd
Luccas Ribeiro Cruz <luccas_185@hotmail.com>
2023-03-30 17:50:07 -0300

    refactor: change effect in label

--- src/components/input-theme.tsx
+++ src/components/input-theme.tsx
@@ -1,5 +1,5 @@
+import { useState } from "react";
 import styles from "./input-theme.module.css";
-
 interface InputThemeProps {
   labelName: string;
   placeHolderName: string;
@@ -11,6 +11,7 @@ export function InputTheme({
   placeHolderName,
   name,
 }: InputThemeProps) {
+  const [inFocus, setInFocus] = useState(false);
   function handleHeigthInput() {
     const inputDoc = document.getElementById(name);
 
@@ -18,20 +19,27 @@ export function InputTheme({
       inputDoc.style.height = inputDoc.scrollHeight + "px";
     });
   }
+
   function addTextShadow() {
     const labelTag = document.getElementById(`${name}${styles.labelTheme}`);
+    setInFocus(true);
     labelTag!.style.textShadow = "1px 1px 2px gray";
   }
+
   function removeTextShadow() {
     const labelTag = document.getElementById(`${name}${styles.labelTheme}`);
+    setInFocus(false);
     labelTag!.style.textShadow = "inherit";
   }
+
   return (
-    <div className={styles.contentInputTheme}>
+    <div className={styles.contentInputTheme} id={`content${name}`}>
       <label
         htmlFor={name}
         id={`${name}${styles.labelTheme}`}
-        className={styles.labelTheme}
+        className={`${styles.labelTheme} ${
+          inFocus ? styles.contentInputFocus : ""
+        }`}
       >
         {labelName}
       </label>
